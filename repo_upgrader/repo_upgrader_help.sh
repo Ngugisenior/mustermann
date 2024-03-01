65e0b3928d33e4dfbff37a55abc331fa3358d12c
Joseph Ngugi Muiruri <NGUGIJOSEPH992@PROTONMAIL.CH>
2024-03-02 00:08:27 +0100

    repo_manager module

--- repo_upgrader/repo_upgrader_help.sh
+++ repo_upgrader/repo_upgrader_help.sh
@@ -19,127 +19,7 @@
 
 choices=("V" "U" "u" "v" "C" "c")
 
-printf "Do you want to [C]reate/([U]pgrade/[V]ersion)\n"
-read choice_input
 
-if [[ ! " ${choices[@]} " =~ " ${choice_input} " ]];then
-	printf "\nUnknown Choice\n"
-else
-
-	printf "Specify one database type:\n1) SAP HANA\n2) Microsoft SQL Server\n3) Oracle\n4) Sybase\n5) SQL Anywhere\n6)IBM DB2\n7) MySQL\n"
-
-	read database_choice
-
-	echo "Provide the Database Server Name:"
-	read database_server_name
-
-	echo "Database port: "
-	read database_port
-
-	echo "Database Username"
-	read database_username
-
-	read -s -p "Database Password: " database_password
-
-	printf "\n"
-
-	read -p "Enter comma separated repository names: " repo_input
-
-	IFS=','
-
-	read -ra database_repo <<< "${repo_input}"
-
-	printf "\nDatabase repository type [local,central]\n"
-	read database_repo_type
-
-	for repo in "${database_repo[@]}";do
-		printf "\n${repo}\n"
-		if [[ $(echo "${choice_input}" | tr '[:lower:]' '[:upper:]' ) == "U" ]]; then
-			case $database_choice in
-				"1")
-					SAP_HANA_Upgrade "${database_username}" "${database_password}" "${database_type}" ${database_server_name} "${database_port}" "${repo}" "${database_repo_type}"
-				;;
-				"2")
-					Microsoft_SQL_Server_Upgrade
-				;;
-				"3")
-					Oracle_Upgrade
-				;;
-				"4")
-					Sybase_Upgrade
-				;;
-				"5")
-					SQL_Anywhere_Upgrade
-				;;
-				"6")
-					IBM_DB2_Upgrade
-				;;
-				"7")
-					MySQL_Upgrade
-				;;
-				*)
-					printf "\nUnknown Choice\n"
-				;;
-			esac
-		elif [[ $(echo "${choice_input}" | tr '[:lower:]' '[:upper:]' ) == "V" ]];then
-			case $database_choice in
-				"1")
-					SAP_HANA_Version_Check "${database_username}" "${database_password}" "${database_type}" ${database_server_name} "${database_port}" "${repo}" "${database_repo_type}"
-				;;
-				"2")
-					Microsoft_SQL_Server_Version_Check
-				;;
-				"3")
-					Oracle_Version_Check
-				;;
-				"4")
-					Sybase_Version_Check
-				;;
-				"5")
-					SQL_Anywhere_Version_Check
-				;;
-				"6")
-					IBM_DB2_Version_Check
-				;;
-				"7")
-					MySQL_Version_Check
-				;;
-				*)
-					printf "\nUnknown Choice\n"
-				;;
-			esac
-		elif [[ $(echo "${choice_input}" | tr '[:lower:]' '[:upper:]' ) == "C" ]];then
-			case $database_choice in
-				"1")
-					SAP_HANA_Repo_Creation "${database_username}" "${database_password}" "${database_type}" ${database_server_name} "${database_port}" "${repo}" "${database_repo_type}"
-				;;
-				"2")
-					Microsoft_SQL_Repo_Creation
-				;;
-				"3")
-					Oracle_Repo_Creation
-				;;
-				"4")
-					Sybase_Repo_Creation
-				;;
-				"5")
-					SQL_Anywhere_Repo_Creation
-				;;
-				"6")
-					IBM_DB2_Repo_Creation
-				;;
-				"7")
-					MySQL_Repo_Creation
-				;;
-				*)
-					printf "\nUnknown Choice\n"
-				;;
-			esac
-		else
-			printf "\nUnknown choice\n"
-		fi
-	done
-fi
 
 
 ####################################################### SAP HANA ##########################################################################################
@@ -279,3 +159,131 @@ function MySQL_Version_Check(){
 	$LINK_DIR/bin/repoman -U"${database_username}" -P"${database_password}" -N"${database_type}" -S${database_server_name} -p"${database_port}" -Q"${repo}" -t"${database_repo_type}" -s -v
 	printf "\n$LINK_DIR/bin/repoman -U"${database_username}" -P"${database_password}" -N"${database_type}" -S${database_server_name} -p"${database_port}" -Q"${repo}" -t"${database_repo_type}" -s -v \n"
 }
+
+
+
+function repo_manager(){
+	printf "Do you want to [C]reate/([U]pgrade/[V]ersion)\n"
+	read choice_input
+
+	if [[ ! " ${choices[@]} " =~ " ${choice_input} " ]];then
+		printf "\nUnknown Choice\n"
+	else
+
+		printf "Specify one database type:\n1) SAP HANA\n2) Microsoft SQL Server\n3) Oracle\n4) Sybase\n5) SQL Anywhere\n6)IBM DB2\n7) MySQL\n"
+
+		read database_choice
+
+		echo "Provide the Database Server Name:"
+		read database_server_name
+
+		echo "Database port: "
+		read database_port
+
+		echo "Database Username"
+		read database_username
+
+		read -s -p "Database Password: " database_password
+
+		printf "\n"
+
+		read -p "Enter comma separated repository names: " repo_input
+
+		IFS=','
+
+		read -ra database_repo <<< "${repo_input}"
+
+		printf "\nDatabase repository type [local,central]\n"
+		read database_repo_type
+
+		for repo in "${database_repo[@]}";do
+			printf "\n${repo}\n"
+			if [[ $(echo "${choice_input}" | tr '[:lower:]' '[:upper:]' ) == "U" ]]; then
+				case $database_choice in
+					"1")
+						SAP_HANA_Upgrade "${database_username}" "${database_password}" "${database_type}" ${database_server_name} "${database_port}" "${repo}" "${database_repo_type}"
+					;;
+					"2")
+						Microsoft_SQL_Server_Upgrade
+					;;
+					"3")
+						Oracle_Upgrade
+					;;
+					"4")
+						Sybase_Upgrade
+					;;
+					"5")
+						SQL_Anywhere_Upgrade
+					;;
+					"6")
+						IBM_DB2_Upgrade
+					;;
+					"7")
+						MySQL_Upgrade
+					;;
+					*)
+						printf "\nUnknown Choice\n"
+					;;
+				esac
+			elif [[ $(echo "${choice_input}" | tr '[:lower:]' '[:upper:]' ) == "V" ]];then
+				case $database_choice in
+					"1")
+						SAP_HANA_Version_Check "${database_username}" "${database_password}" "${database_type}" ${database_server_name} "${database_port}" "${repo}" "${database_repo_type}"
+					;;
+					"2")
+						Microsoft_SQL_Server_Version_Check
+					;;
+					"3")
+						Oracle_Version_Check
+					;;
+					"4")
+						Sybase_Version_Check
+					;;
+					"5")
+						SQL_Anywhere_Version_Check
+					;;
+					"6")
+						IBM_DB2_Version_Check
+					;;
+					"7")
+						MySQL_Version_Check
+					;;
+					*)
+						printf "\nUnknown Choice\n"
+					;;
+				esac
+			elif [[ $(echo "${choice_input}" | tr '[:lower:]' '[:upper:]' ) == "C" ]];then
+				case $database_choice in
+					"1")
+						SAP_HANA_Repo_Creation "${database_username}" "${database_password}" "${database_type}" ${database_server_name} "${database_port}" "${repo}" "${database_repo_type}"
+					;;
+					"2")
+						Microsoft_SQL_Repo_Creation
+					;;
+					"3")
+						Oracle_Repo_Creation
+					;;
+					"4")
+						Sybase_Repo_Creation
+					;;
+					"5")
+						SQL_Anywhere_Repo_Creation
+					;;
+					"6")
+						IBM_DB2_Repo_Creation
+					;;
+					"7")
+						MySQL_Repo_Creation
+					;;
+					*)
+						printf "\nUnknown Choice\n"
+					;;
+				esac
+			else
+				printf "\nUnknown choice\n"
+			fi
+		done
+	fi
+}
+
+repo_manager
